c5579fb9a99da5aae16fc4f40d7cd038709ef74a
Miguel <miguelrodriguez2058@gmail.com>
2026-03-30 15:17:04 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Socialcademy
+============
+
+Social cademy app from Codecademy.
+
+This project is a socialmedia app project.
+
+**Technologies Used**
+
+* Firebase
+* Dependencies with Swift Package Manager
---
+++ Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,22 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by Miguel Rodriguez on 3/30/26.
+//
+
+import SwiftUI
+import Firebase
+
+@main
+struct SocialcademyApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
